817d0df49f74d8d8181aef95e7e0c33b9f98f361
Cole Haus <colehaus@cryptolab.net>
2018-04-13 23:59:57 -0700

    Remove extraneous type variable

--- src/Data/Yaml.purs
+++ src/Data/Yaml.purs
@@ -8,7 +8,7 @@ import Data.Function.Uncurried (Fn3, runFn3)
 
 foreign import parseFromYamlImpl :: forall l r. Fn3 (l -> Either l r) (r -> Either l r) String (Either String Json)
 
-parseFromYaml :: forall a. String -> Either String Json
+parseFromYaml :: String -> Either String Json
 parseFromYaml = runFn3 parseFromYamlImpl Left Right
 
 foreign import printToYaml :: Json -> String
