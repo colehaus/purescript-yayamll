5f1832477bb7fff0ad6d3ef02fdc4292ec727c55
Cole Haus <colehaus@cryptolab.net>
2018-04-13 23:55:08 -0700

    Initial commit

---
+++ src/Data/Yaml.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var yaml = require('js-yaml');
+
+exports.parseFromYamlImpl = function(left, right, string) {
+  try {
+    return right(yaml.safeLoad(string));
+  } catch (e) {
+    return left(e.toString());
+  }
+};
+
+exports.printToYaml = function(json) {
+  return yaml.safeDump(json, { noRefs: true, lineWidth: -1, noCompatMode: true });
+};
---
+++ src/Data/Yaml.purs
@@ -0,0 +1,14 @@
+module Data.Yaml (parseFromYaml, printToYaml) where
+
+import Prelude
+
+import Data.Argonaut.Core (Json)
+import Data.Either (Either(..))
+import Data.Function.Uncurried (Fn3, runFn3)
+
+foreign import parseFromYamlImpl :: forall l r. Fn3 (l -> Either l r) (r -> Either l r) String (Either String Json)
+
+parseFromYaml :: forall a. String -> Either String Json
+parseFromYaml = runFn3 parseFromYamlImpl Left Right
+
+foreign import printToYaml :: Json -> String
